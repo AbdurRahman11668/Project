4dd6582094c840b40852d8514c25234dea0a314d
Abdur Rahman <abdur1222.ar@gmail.com>
2024-12-04 22:41:21 -0500

    first commit

--- Website/app.py
+++ Website/app.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template, session, render_template, request, g, jsonify
 import sqlite3
-import random
 import os
+import credentials
 
 app = Flask(__name__)
 
-# SQLite connection function
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect('Videogames.db')
-#     return db
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the directory of app.py
-DATABASE = os.path.join(BASE_DIR, '..', 'Videogames.db')  # Go one level up to find database.db
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = os.path.join(BASE_DIR, '..', 'Videogames.db') 
+
 
 
 # Function to connect to the database
 def get_db():
-    connection = sqlite3.connect(DATABASE)
-    connection.row_factory = sqlite3.Row 
-    return connection
+    try:
+        connection = sqlite3.connect(DATABASE)
+        connection.row_factory = sqlite3.Row
+        return connection
+    except sqlite3.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', 
+                           data_source="Source of your data (e.g., a public dataset)",
+                           variables={"Column1": "Description 1", "Column2": "Description 2"})
 
 # Route for the About page
 @app.route('/about')
@@ -29,28 +36,27 @@ def about():
                            data_source="Source of your data (e.g., a public dataset)",
                            variables={"Column1": "Description 1", "Column2": "Description 2"})
 
-# Route for the Data page
-# @app.route('/data')
-# def data():
-#     conn = get_db()
-#     cursor = conn.execute('SELECT * FROM VideoGameStocks')
-#     rows = cursor.fetchall()
-#     conn.close()
-#     return render_template('data.html', rows=rows)
 
 @app.route('/data')
 def data():
     try:
         conn = get_db()
-        cursor = conn.execute('SELECT * FROM VideoGameStocks LIMIT 20')
+        cursor = conn.execute('SELECT * FROM VideoGameStocks')
         rows = cursor.fetchall()
-        print(rows)  # Debug: check if rows contain data
+        print(rows)
     except sqlite3.Error as e:
         print(f"Database error: {e}")
-        rows = []  # Fallback to an empty list if there's an error
+        rows = [] 
     finally:
         conn.close()
     return render_template('data.html', rows=rows)
+
+# api-key route
+
+@app.route('/weather')
+def weather():
+    api_key = credentials.weather_api_key
+    return f"Your Weather API Key is: {api_key}"
 
 
 if __name__ == '__main__':
